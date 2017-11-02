b201ec3ae42eba8bfb475e8391a3217bfa8db714
Devin Richards <devin@geekosphere.net>
2017-11-02 09:30:49 -0400

    vim settings

---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+# pathogen loader
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# solarized themes
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized/
+
