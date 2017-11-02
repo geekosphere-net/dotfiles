df80364892b513f3b1271717b49c87fce953f220
Devin Richards <devin@geekosphere.net>
2017-11-02 09:12:34 -0400

    set zsh options

---
+++ zsh/config.zsh
@@ -0,0 +1,23 @@
+export VISUAL=vim
+export EDITOR=vim
+
+bindkey -e
+
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS
+setopt SHARE_HISTORY
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
+
+setopt HIST_VERIFY
+setopt EXTENDED_HISTORY
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+
+setopt complete_aliases
+
