734203eff77b3752127bbd8345ad5eaca5a3bc90
Devin Richards <devin@geekosphere.net>
2017-11-02 08:50:06 -0400

    removed zsh-syntax-highlighting

--- configure.sh
+++ configure.sh
@@ -39,7 +39,7 @@ echo "Now installing oh-my-zsh plugins..."
 echo ''
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Bash color scheme
 echo ''
