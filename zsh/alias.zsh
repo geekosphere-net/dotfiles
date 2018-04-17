15a26f0509805293c4f643a2cb140f67d57d8bc8
Devin Richards <devin@geekosphere.net>
2018-04-17 12:19:58 -0400

    added markloop()

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,3 +8,14 @@ function mark {
 	$*
 }
 
+function markloop () {
+        echo -ne "\e];$*\a"
+        /usr/bin/figlet -f big -Wc $(date +%T)
+        local startT=$(date +%s)
+        while true
+        do
+                $*
+                sleep 10
+                /usr/bin/figlet -f small -Wl $(( $(date +%s) - $startT ))
+        done
+}
