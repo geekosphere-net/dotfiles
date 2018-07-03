fd7e92887383b361a76a4695b1c8d88839d718ee
Devin Richards <devin@geekosphere.net>
2018-07-03 08:57:12 -0400

    added pkg install completion

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,1 +1,3 @@
 [[ -s ~/.local/bin/aws_zsh_completer.sh ]] && source ~/.local/bin/aws_zsh_completer.sh
+
+[[ -s /usr/share/zsh/vendor-completions/_awscli ]] && source /usr/share/zsh/vendor-completions/_awscli
