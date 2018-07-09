0411ff3c316ee3976949ce699e9e94b6f44e5416
Devin Richards <devin@geekosphere.net>
2018-07-09 14:55:17 -0400

    updated location of completion file

--- Bluemix/autocomplete.zsh
+++ Bluemix/autocomplete.zsh
@@ -1,1 +1,2 @@
 [[ -f /usr/local/Bluemix/bx/zsh_autocomplete ]] && source /usr/local/Bluemix/bx/zsh_autocomplete
+[[ -f /usr/local/ibmcloud/bx/zsh_autocomplete ]] && source /usr/local/ibmcloud/bx/zsh_autocomplete
