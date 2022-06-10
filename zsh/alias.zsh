69e142f844ab98f0602a6732f606329f5c3dac0e
Devin Richards <devin.richards@alcon.com>
2022-06-10 16:58:22 -0400

    added more docker aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,6 +2,15 @@
 alias ls='ls -FC --color=auto'
 
 
+alias mvnn="docker run -v ~/.m2:/var/maven/.m2 -v \$PWD:/usr/src/app -w /usr/src/app -ti --rm -u $(id -u ${whoami}):$(id -g ${whoami}) -e MAVEN_CONFIG=/var/maven/.m2 maven:3-jdk-8-slim mvn -Duser.home=/var/maven "
+alias mulemvn="docker run -v /mnt/c/Users/drich/.m2:/var/maven/.m2 -v \$PWD:/usr/src/app -w /usr/src/app -ti --rm -u $(id -u ${whoami}):$(id -g ${whoami}) -e MAVEN_CONFIG=/var/maven/.m2 maven:3-jdk-8-slim mvn versions:display-dependency-updates --show-version -Denvironment.id=6913d3b1-798c-472e-a9a5-a5224230bed1 -Ddomain=localhost -Dworker.id=0 -Denv=dev -Dsecret.key=abcd1234abcd1234 -Duser.home=/var/maven "
+alias anypoint-cli="docker run --rm --name anypoint-cli -it integrational/anypoint-cli:latest "
+
+alias xmllint="docker run -i --rm --name 'xmllint' -v \$PWD:/data mribeiro/xmllint  "
+
+alias yq="docker run -v \$PWD:/workdir -it --rm mikefarah/yq:4 \$@"
+alias aws="docker run --rm -it -v ~/.aws:/root/.aws -v \$PWD:/aws -e AWS_PROFILE -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e TZ amazon/aws-cli"
+
 function mark {
 	echo -ne "\e];$*\a"
 	/usr/bin/figlet -f big -Wc $(date +%T)
