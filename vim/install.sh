9cc5f4b484307644f3f2bdd1446c856516972676
Devin Richards <drichards@eliassen.com>
2026-06-05 10:48:03 -0400

    updated vim colors/config

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,8 @@
 # pathogen loader
-mkdir -p ~/.vim/autoload ~/.vim/bundle 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#mkdir -p ~/.vim/autoload ~/.vim/bundle 
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # solarized themes
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized/
+#git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized/
+curl -fLo ~/.vim/colors/solarized.vim --create-dirs https://raw.githubusercontent.com/ericbn/vim-solarized/master/colors/solarized.vim
 
